575105291073aec260465cae7b3e5182ea2a8ca3
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-11-07 03:19:59 +0200

    done HW-2-feedback

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,22 @@
+import style from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export default function Statistics({ good, neutral, bad, total, PositiveFeedback }) {
+  return (
+    <ul className={style.items_list}>
+          <li className={style.item}>Good: {good}</li>
+          <li className={style.item}>Neutural: {neutral}</li>
+          <li className={style.item}>Bad: {bad}</li>
+          <li className={style.item}>Total: {total}</li>
+          <li className={style.item}>Positive Feedback: {PositiveFeedback}</li>
+    </ul>
+  );
+}
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  PositiveFeedback: PropTypes.number.isRequired,
+};
